85a2123a2f282cc6bc932eb9761b748f9faaf82c
Albuja, Juan <juansealbuja@gmail.com>
2022-06-01 20:15:39 -0500

    adding a write instruction on the wait-for-it script to test if it's being executed on runtime.

--- wordpress/wait-for-it.sh
+++ wordpress/wait-for-it.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "Wait for it script is now running!"
+echo "Wait for it script ran successfully!" > wait-for-it-result
 timeout 15 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' mysqldb 3306
